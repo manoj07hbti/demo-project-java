0bebce771e19fbe9d7cb77d58b60c8de8f1e6c8c
60manojkumar@gmail.com <Motivated@2019>
2021-10-28 20:06:06 +0530

    Initial commit

---
+++ src/DemoFirstClass.java
@@ -0,0 +1,10 @@
+public class DemoFirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my first java Class...");
+        System.out.println("Second line ...");
+        System.out.println("I am Java Developer...");
+
+    }
+}
