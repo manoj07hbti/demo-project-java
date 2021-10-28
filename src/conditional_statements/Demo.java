0cecdcc898b23d927e63717de5df717f460e047a
60manojkumar@gmail.com <Motivated@2019>
2021-10-28 20:15:49 +0530

    if and else

---
+++ src/conditional_statements/Demo.java
@@ -0,0 +1,30 @@
+package conditional_statements;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int age=10;
+
+        if(age>18){
+
+            System.out.println("Student is Young ....");
+        }
+        else
+        {
+            System.out.println("Student is child");
+        }
+
+        String city="Delhi";
+
+        if(city=="Agra"){
+
+            System.out.println("You are in Agra .....");
+        }
+        else
+        {
+            System.out.println("You are Not in Agra .....");
+        }
+
+    }
+}
